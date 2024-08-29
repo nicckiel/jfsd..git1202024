7e5d5c2b7b05bc1357ae8a33d577c24b27e1d6f0
KORDORWU PATRICK <niccomplex@gmail.com>
2024-08-29 21:00:47 +0200

    HEAD

---
+++ WelcomeApp/src/DataTypes.java
@@ -0,0 +1,23 @@
+
+public class DataTypes {
+  public static void main(String[] args) {
+	int a;
+	int b=100;
+	double c=100.40;
+	char d ='£';
+	boolean result = true;
+	// System.out.println(a);
+	 System.out.println(b);
+	 System.out.println("b");
+	 System.out.println("Value of b is :"+b);
+	 System.out.println("Value of b is :"+c);
+	 
+	 System.out.println("Value of b is :"+d);
+	 
+	 System.out.println("Value of b is :"+b);
+	 System.out.println("Value of b is :"+result);
+	 
+	 
+    }
+
+}
---
+++ WelcomeApp/src/Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Java");
+		System.out.print("Welcometo Java");
+		System.out.println("Welcome to java");
+		System.out.println("Welcome again to Java");
+			
+	}
+
+}

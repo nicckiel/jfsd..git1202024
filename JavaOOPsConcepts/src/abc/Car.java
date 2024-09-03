b3713d0f6e2ff88891a4f9c597ad64c7994333d0
nicckiel <niccomplex@gmail.com>
2024-09-03 11:48:09 +0200

    done

---
+++ JavaOOPsConcepts/src/abc/Car.java
@@ -0,0 +1,24 @@
+package abc; //java.util
+
+public class Car {
+	//Property (declaration and initialization)
+	 int wheel;
+	 String colour;
+	 float price;
+	 
+	 //Behaviours ( method)
+	 
+     void start() {
+    	 System.out.println("Car start");
+     }
+      void appliedGear() {
+    	  System.out.println("applied gear");
+      }
+     void driveOn() {
+    	 System.out.println("drivngOn car");
+     }
+     void stopping() {
+    	 System.out.println("stopiing car");
+     }
+     
+}
---
+++ JavaOOPsConcepts/src/abc/TesingCar.java
@@ -0,0 +1,15 @@
+package abc;
+
+public class TesingCar {
+
+	public static void main(String[] args) {
+		// Create Object as method to Access class Car
+		Car pegeaot = new Car(); // heap memory created
+		pegeaot.start();
+		pegeaot.appliedGear();
+		pegeaot.driveOn();
+		pegeaot.stopping();
+		
+	}
+
+}
